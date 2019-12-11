bac79a9fc270dac830d57ba212deb1aad99d7ad6
Emelie Petersson <54805510+emeliepetersson@users.noreply.github.com>
2019-12-11 09:22:59 +0100

    add public

---
+++ Public/views/navigation.php
@@ -0,0 +1,39 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#"><img src="<?php echo $config['logo']; ?>" alt="Picture this logo" width="40%"></a>
+
+    <ul class="navbar-nav">
+
+
+
+        <?php if (isset($_SESSION['user'])) : ?>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Profile</a>
+            </li><!-- /nav-item -->
+            <li class="nav-item">
+                <a class="nav-link" href="#">Settings</a>
+            </li><!-- /nav-item -->
+            <li class="nav-item">
+                <a class="nav-link" href="#">Upload post</a>
+            </li><!-- /nav-item -->
+            <li class="nav-item">
+                <a class="nav-link" href="/app/users/logout.php">Logout</a>
+            </li><!-- /nav-item -->
+        <?php else : ?>
+            <li class="nav-item">
+                <a class="nav-link" href="/index.php">Home</a>
+            </li><!-- /nav-item -->
+            <li class="nav-item">
+                <a class="nav-link" href="/about.php">About</a>
+            </li><!-- /nav-item -->
+            <li class="nav-item">
+                <a class="nav-link <?php echo $_SERVER['SCRIPT_NAME'] === '/login.php' ? 'active' : ''; ?>" href="login.php">Login</a>
+            </li><!-- /nav-item -->
+            <li class="nav-item">
+                <a class="nav-link" href="/signup.php">Sign up</a>
+            </li><!-- /nav-item -->
+        <?php endif; ?>
+
+
+
+    </ul><!-- /navbar-nav -->
+</nav><!-- /navbar -->
