225a7868b4e859b80d86b8eb669b74def5147333
Emelie Petersson <54805510+emeliepetersson@users.noreply.github.com>
2019-12-11 11:38:30 +0100

    add sign up logic

--- Public/app/users/signup.php
+++ Public/app/users/signup.php
@@ -6,5 +6,44 @@ require __DIR__ . '/../autoload.php';
 
 //In this file we sign up users.
 
+if (isset($_POST['email'], $_POST['first-name'], $_POST['last-name'], $_POST['password'])) {
+    $email = trim(filter_var($_POST['email'], FILTER_SANITIZE_EMAIL));
+    $firstName = trim(filter_var($_POST['first-name'], FILTER_SANITIZE_STRING));
+    $lastName = trim(filter_var($_POST['last-name'], FILTER_SANITIZE_STRING));
+    $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
+    $errors = [];
 
-//Add error message if email already exists.
+    $statement = $pdo->prepare('SELECT email FROM users WHERE email = :email');
+    $statement->bindParam(':email', $email, PDO::PARAM_STR);
+    $statement->execute();
+    $storedEmail = $statement->fetch(PDO::FETCH_ASSOC);
+
+    if ($storedEmail) { //If $storedEmail exists an error message will be printed
+        $errors[] = "The email already exist!";
+    }
+
+    if (count($errors) > 0) {
+        $_SESSION['errors'] = $errors;
+        redirect('/signup.php');
+        exit;
+    }
+
+    // If email doesn't already exist, insert the data into the user table.
+    $query = 'INSERT INTO users (email, first_name, last_name, password) VALUES (:email, :first_name, :last_name, :password)';
+
+    $statement = $pdo->prepare($query);
+
+    if (!$statement) {
+        die(var_dump($pdo->errorInfo()));
+    }
+
+    $statement->bindParam(':email', $email, PDO::PARAM_STR);
+    $statement->bindParam(':first_name', $firstName, PDO::PARAM_STR);
+    $statement->bindParam(':last_name', $lastName, PDO::PARAM_STR);
+    $statement->bindParam(':password', $password, PDO::PARAM_STR);
+    $statement->execute();
+}
+
+// We should put this redirect in the end of this file since we always want to
+// redirect the user back from this file. We don't know
+redirect('/../../login.php');
