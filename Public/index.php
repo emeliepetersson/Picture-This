6b5fd01b154a5baa35c283122d779bc39dc87c61
emeliepetersson <emepet1204@skola.goteborg.se>
2019-12-11 09:22:59 +0100

    add public

---
+++ Public/index.php
@@ -0,0 +1,9 @@
+<?php require __DIR__ . '/views/header.php'; ?>
+
+<article>
+    <h1><img src="<?php echo $config['logo']; ?>" alt="Picture this logo"></h1>
+    <a href="login.php">Login</a>
+    <a href="signup.php">Sign up</a>
+</article>
+
+<?php require __DIR__ . '/views/footer.php'; ?>
---
+++ Public/signup.php
@@ -0,0 +1,35 @@
+<?php require __DIR__ . '/views/header.php'; ?>
+
+<article>
+    <h1>Create account</h1>
+
+    <form action="app/users/login.php" method="post">
+        <div class="form-group">
+            <label for="email">Email</label>
+            <input class="form-control" type="email" name="email" placeholder="example@email.com" required>
+            <small class="form-text text-muted">Please provide the your email address.</small>
+        </div><!-- /form-group -->
+
+        <div class="form-group">
+            <label for="first-name">First name</label>
+            <input class="form-control" type="name" name="first-name" placeholder="First name" required>
+            <small class="form-text text-muted">Please provide the your first name.</small>
+        </div><!-- /form-group -->
+
+        <div class="form-group">
+            <label for="last-name">Last name</label>
+            <input class="form-control" type="name" name="last-name" placeholder="Last name" required>
+            <small class="form-text text-muted">Please provide the your last name.</small>
+        </div><!-- /form-group -->
+
+        <div class="form-group">
+            <label for="password">Password</label>
+            <input class="form-control" type="password" name="password" required>
+            <small class="form-text text-muted">Please provide your password.</small>
+        </div><!-- /form-group -->
+
+        <button type="submit" class="btn btn-primary">Create account</button>
+    </form>
+</article>
+
+<?php require __DIR__ . '/views/footer.php'; ?>
