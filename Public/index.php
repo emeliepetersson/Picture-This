be0612bf0b3b4d628291fad10b630bb10a9f2024
Emelie Petersson <54805510+emeliepetersson@users.noreply.github.com>
2019-12-12 09:40:05 +0100

    add bottom bar

--- Public/index.php
+++ Public/index.php
@@ -1,9 +1,15 @@
 <?php require __DIR__ . '/views/header.php'; ?>
 
 <article>
-    <h1><img src="<?php echo $config['logo']; ?>" alt="Picture this logo"></h1>
-    <a href="login.php">Login</a>
-    <a href="signup.php">Sign up</a>
+    <?php if (isset($_SESSION['user'])) : ?>
+        <h1>Photos feed</h1>
+        <p>There is no photos...</p>
+        <?php require __DIR__ . '/views/bottom-bar.php'; ?>
+    <?php else : ?>
+        <h1><img src="<?php echo $config['logo']; ?>" alt="Picture this logo"></h1>
+        <a href="login.php">Login</a>
+        <a href="signup.php">Sign up</a>
+    <?php endif; ?>
 </article>
 
 <?php require __DIR__ . '/views/footer.php'; ?>
--- Public/views/header.php
+++ Public/views/header.php
@@ -2,7 +2,7 @@
 // Always start by loading the default application setup.
 require __DIR__ . '/../app/autoload.php';
 
-require __DIR__ . '/errors.php';
+require __DIR__ . '/../app/errors.php';
 
 ?>
 <!DOCTYPE html>
@@ -16,6 +16,7 @@ require __DIR__ . '/errors.php';
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="/assets/styles/main.css">
+    <link rel="stylesheet" href="/assets/styles/bottom-bar.css">
 </head>
 
 <body>
