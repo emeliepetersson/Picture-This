a9f8f3afaaca6b0c658c1cabd3fae9e68295201c
emeliepetersson <emepet1204@skola.goteborg.se>
2019-12-12 11:39:22 +0100

    add preview function

--- Public/upload.php
+++ Public/upload.php
@@ -2,11 +2,16 @@
 
 <div class="upload-post">
     <h1>Upload post</h1>
-    <form action="upload.php" method="post" enctype="multipart/form-data">
+    <form action="upload.php" method="post" enctype="multipart/form-data" id="post">
         <label for="image">Choose a image to upload</label>
-        <input type="file" name="image" id="image" required>
+        <input type="file" name="image" id="image" class="choose-file" required>
+
+        <!--Preview image -->
+        <img id="output-image" alt="image preview" />
+
         <button type="submit">Upload</button>
     </form>
+    <textarea name="description" form="post" maxlength="255"></textarea>
 </div>
 
 <?php require __DIR__ . '/views/bottom-bar.php'; ?>
