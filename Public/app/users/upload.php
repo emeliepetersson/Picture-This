b8a543077129f885841d1cab8ad5d902f0d69a5b
Emelie Petersson <54805510+emeliepetersson@users.noreply.github.com>
2019-12-12 15:10:20 +0100

    update upload.php

--- Public/app/users/upload.php
+++ Public/app/users/upload.php
@@ -4,11 +4,12 @@ declare(strict_types=1);
 
 require __DIR__ . '/../autoload.php';
 
-if (isset($_FILES['image'])) {
+if (isset($_FILES['image'], $_POST['description'])) {
     $image = $_FILES['image'];
+    $description = filter_var($_POST['description'], FILTER_SANITIZE_STRING);
     $errors = [];
 
-    if ($image['type'] !== 'image/gif') {
+    if ($image['type'] !== 'image/gif' && $image['type'] !== 'image/jpeg' && $image['type'] !== 'image/png') {
         $errors[] = 'The ' . $image['name'] . ' image file type is not allowed.';
     }
     if ($image['size'] >= 3000000) {
@@ -23,6 +24,10 @@ if (isset($_FILES['image'])) {
 
     $destination = __DIR__ . '/../../uploads/' . uniqid() . $image['name'];
     move_uploaded_file($image['tmp_name'], $destination);
+
+
+    //get user id with $_SESSION[], add $description and image file name to table with users id
+
 }
 
 redirect('/upload.php');
