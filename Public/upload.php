e900c3d6a95eb97c4de26c626dfb4bd451d4ab05
emeliepetersson <emepet1204@skola.goteborg.se>
2019-12-12 10:35:11 +0100

    add upload image form

--- Public/upload.php
+++ Public/upload.php
@@ -1,6 +1,13 @@
 <?php require __DIR__ . '/views/header.php'; ?>
 
+<div class="upload-post">
+    <h1>Upload post</h1>
+    <form action="upload.php" method="post" enctype="multipart/form-data">
+        <label for="image">Choose a image to upload</label>
+        <input type="file" name="image" id="image" required>
+        <button type="submit">Upload</button>
+    </form>
+</div>
+
 <?php require __DIR__ . '/views/bottom-bar.php'; ?>
-
-
 <?php require __DIR__ . '/views/footer.php'; ?>
