d629d0a68cc140f1305bfeb118c920d2673cf96e
Emelie Petersson <54805510+emeliepetersson@users.noreply.github.com>
2019-12-12 11:39:22 +0100

    add preview function

---
+++ Public/assets/scripts/preview-image.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const chooseFile = document.querySelector(".choose-file");
+
+function previewImage() {
+  var preview = document.querySelector("#output-image");
+  var file = document.querySelector("input[type=file]").files[0];
+  var reader = new FileReader();
+
+  reader.addEventListener(
+    "load",
+    function() {
+      preview.src = reader.result;
+    },
+    false
+  );
+
+  if (file) {
+    reader.readAsDataURL(file);
+  }
+}
+
+chooseFile.addEventListener("change", previewImage);
