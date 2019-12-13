73ba549a82054ae56e1ce73025bb728269d55f4e
emeliepetersson <emepet1204@skola.goteborg.se>
2019-12-13 13:54:42 +0100

    insert image and description into posts table

--- Public/app/users/logout.php
+++ Public/app/users/logout.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 require __DIR__ . '/../autoload.php';
 
+if (!isset($_SESSION['user'])) { // Make this into a function!!!
+    redirect('/');
+}
+
 // In this file we logout users.
 
 // Remove the user session variable and redirect the user back to the homepage.
--- Public/app/users/upload.php
+++ Public/app/users/upload.php
@@ -7,6 +7,7 @@ require __DIR__ . '/../autoload.php';
 if (isset($_FILES['image'], $_POST['description'])) {
     $image = $_FILES['image'];
     $description = filter_var($_POST['description'], FILTER_SANITIZE_STRING);
+    $userId = $_SESSION['user']['id'];
     $errors = [];
 
     if ($image['type'] !== 'image/gif' && $image['type'] !== 'image/jpeg' && $image['type'] !== 'image/png') {
@@ -22,12 +23,33 @@ if (isset($_FILES['image'], $_POST['description'])) {
         exit;
     }
 
-    $destination = __DIR__ . '/../../uploads/' . uniqid() . $image['name'];
+    $imageFileName = uniqid() . $image['name'];
+    $destination = __DIR__ . '/../../uploads/' .  $imageFileName;
     move_uploaded_file($image['tmp_name'], $destination);
 
 
     //get user id with $_SESSION[], add $description and image file name to table with users id
+    //image, description, user_id, date
+    $fileNames = scandir(__DIR__ . '/../../uploads');
 
+    foreach ($fileNames as $fileName) {
+        if ($fileName === $imageFileName) {
+
+            $query = 'INSERT INTO posts (image, description, user_id, date) VALUES (:image, :description, :user_id, :date)';
+
+            $statement = $pdo->prepare($query);
+
+            if (!$statement) {
+                die(var_dump($pdo->errorInfo()));
+            }
+
+            $statement->bindParam(':image', $fileName, PDO::PARAM_STR);
+            $statement->bindParam(':description', $description, PDO::PARAM_STR);
+            $statement->bindParam(':user_id', $userId, PDO::PARAM_INT);
+            $statement->bindParam(':date', date('d-m-Y, H:i:s'), PDO::PARAM_STR);
+            $statement->execute();
+        }
+    }
 }
 
 redirect('/upload.php');
