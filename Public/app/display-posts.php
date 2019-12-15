4f4fc27c48340455ad494c9d071424744c4d96dc
emeliepetersson <emepet1204@skola.goteborg.se>
2019-12-15 12:08:05 +0100

    display posts

--- Public/app/display-posts.php
+++ Public/app/display-posts.php
@@ -1,4 +1,10 @@
 <?php
-require __DIR__ . '/autoload.php';
-$posts = getDataFromTable($pdo, 'image, description, date', 'posts', 'user_id', $_SESSION['user']['id']);
-die(var_dump($posts));
+// $posts = getDataFromTable($pdo, 'image, description, date', 'posts', 'user_id', $_SESSION['user']['id']);
+
+$statement = $pdo->query("SELECT image, description, date, first_name, last_name FROM posts INNER JOIN users ON posts.user_id = users.id");
+
+if (!$statement) {
+    die(var_dump($pdo->errorInfo()));
+}
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
