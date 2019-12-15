529bda44cac647fcf1f2feef616e679564a818e8
Emelie Petersson <54805510+emeliepetersson@users.noreply.github.com>
2019-12-15 12:08:05 +0100

    display posts

--- Public/index.php
+++ Public/index.php
@@ -3,8 +3,25 @@
 <article>
     <!-- The view if the user is logged in -->
     <?php if (isset($_SESSION['user'])) : ?>
+
+        <?php require __DIR__ . '/app/display-posts.php'; ?>
         <h1>Photos feed</h1>
-        <p>There is no photos...</p>
+        <?php if ($posts === null) : ?>
+            <p>There is no photos...</p>
+        <?php else : ?>
+            <?php foreach ($posts as $post) : ?>
+                <article>
+                    <header>
+                        <h2>
+                            <?php echo $post['first_name'] . " " . $post['last_name']; ?>
+                        </h2>
+                        <footer><?php echo $post['date'] ?></footer>
+                        <header>
+                            <img src="/uploads/<?php echo $post['image'] ?>" alt="">
+                            <p><?php echo $post['description'] ?></p>
+                </article>
+            <?php endforeach; ?>
+        <?php endif; ?>
         <?php require __DIR__ . '/views/bottom-bar.php'; ?>
 
 
