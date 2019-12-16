93604a266448ca2923de801c9debf5c805491325
Emelie Petersson <54805510+emeliepetersson@users.noreply.github.com>
2019-12-16 11:15:24 +0100

    display posts in profile

--- Public/app/display-posts.php
+++ Public/app/display-posts.php
@@ -1,10 +1,19 @@
 <?php
-// $posts = getDataFromTable($pdo, 'image, description, date', 'posts', 'user_id', $_SESSION['user']['id']);
 
-$statement = $pdo->query("SELECT image, description, date, first_name, last_name FROM posts INNER JOIN users ON posts.user_id = users.id");
+$query = "SELECT image, description, date, first_name, last_name FROM posts INNER JOIN users ON posts.user_id = users.id";
 
-if (!$statement) {
+// Get all posts from logged in user
+$statementOne = $pdo->prepare($query . " WHERE users.id = :id");
+$statementOne->bindParam(':id', $_SESSION['user']['id'], PDO::PARAM_INT);
+$statementOne->execute();
+$userPosts = $statementOne->fetchAll(PDO::FETCH_ASSOC);
+
+
+// Get all posts from all users
+$statementTwo = $pdo->query($query);
+
+if (!$statementTwo) {
     die(var_dump($pdo->errorInfo()));
 }
 
-$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+$allPosts = $statementTwo->fetchAll(PDO::FETCH_ASSOC);
---
+++ Public/profile.php
@@ -0,0 +1,31 @@
+<?php
+require __DIR__ . '/views/header.php';
+
+if (!isset($_SESSION['user'])) { // Make this into a function!!!
+    redirect('/');
+}
+
+?>
+
+<header>
+    <h1><?php echo $_SESSION['user']['first_name'] . " " . $_SESSION['user']['last_name']; ?></h1>
+</header>
+        <?php require __DIR__ . '/app/display-posts.php'; ?>
+        <?php if (!$userPosts) : ?>
+            <p>There is no photos...</p>
+            <?php else : ?>
+                <?php foreach ($userPosts as $post) : ?>
+            <article>
+                <header>
+                    <h2>
+                        <?php echo $post['first_name'] . " " . $post['last_name']; ?>
+                    </h2>
+                    <footer><?php echo $post['date'] ?></footer>
+                    <header>
+                        <img src="/uploads/<?php echo $post['image'] ?>" alt="">
+                        <p><?php echo $post['description'] ?></p>
+            </article>
+        <?php endforeach; ?>
+        <?php endif; ?>
+        <?php require __DIR__ . '/views/bottom-bar.php'; ?>
+
