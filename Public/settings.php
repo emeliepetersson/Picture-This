1155ad16258c2dee0b1e8e5a21fac1890fd0ef5c
emeliepetersson <emepet1204@skola.goteborg.se>
2019-12-16 11:31:02 +0100

    add form to setting.php

---
+++ Public/settings.php
@@ -0,0 +1,66 @@
+<?php
+require __DIR__ . '/views/header.php';
+
+if (!isset($_SESSION['user'])) { // Make this into a function!!!
+    redirect('/');
+}
+
+?>
+
+<article>
+    <h1>Settings</h1>
+
+    <form action="app/users/edit-settings.php" method="post">
+        <?php foreach ($errors as $error) : ?>
+            <div class="alert alert-danger">
+                <?php echo $error; ?>
+            </div><!-- /alert -->
+        <?php endforeach; ?>
+        <div class="form-group">
+            <label for="email">Email</label>
+            <input class="form-control" type="email" name="email" value="<?php echo $_SESSION['user']['email'] ?>">
+            <small class="form-text text-muted">Please provide your email address.</small>
+        </div><!-- /form-group -->
+
+        <div class="form-group">
+            <label for="first-name">First name</label>
+            <input class="form-control" type="name" name="first-name" value="<?php echo $_SESSION['user']['first_name'] ?>">
+            <small class="form-text text-muted">Please provide your first name.</small>
+        </div><!-- /form-group -->
+
+        <div class="form-group">
+            <label for="last-name">Last name</label>
+            <input class="form-control" type="name" name="last-name" value="<?php echo $_SESSION['user']['last_name'] ?>">
+            <small class="form-text text-muted">Please provide the your last name.</small>
+        </div><!-- /form-group -->
+
+        <div class="form-group">
+            <label for="password">Password</label>
+            <input class="form-control" type="password" name="password">
+            <small class="form-text text-muted">Please provide your new password.</small>
+        </div><!-- /form-group -->
+
+        <div class="form-group">
+            <label for="password">Confirm password</label>
+            <input class="form-control" type="password" name="confirm-password">
+            <small class="form-text text-muted">Please confirm your new password.</small>
+        </div><!-- /form-group -->
+
+        <label for="image">Choose a profile image to upload</label>
+        <input type="file" name="image" id="image" class="choose-file">
+
+        <!--Preview image -->
+        <div class="form-group">
+            <img id="output-image" alt="image preview" />
+        </div>
+
+        <div class="form-group">
+            <small class="form-text text-muted">Write your biography.</small>
+            <textarea name="description" form="post" maxlength="255"></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary">Update Settings</button>
+    </form>
+</article>
+
+<?php require __DIR__ . '/views/bottom-bar.php'; ?>
+<?php require __DIR__ . '/views/footer.php'; ?>
