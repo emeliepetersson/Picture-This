cbae42323c1f7aa5e79a279fa976acef6b52f9f0
Emelie Petersson <54805510+emeliepetersson@users.noreply.github.com>
2019-12-20 16:35:55 +0100

    add new functions

--- Public/index.php
+++ Public/index.php
@@ -1,9 +1,11 @@
-<?php require __DIR__ . '/views/header.php'; ?>
+<?php
+require __DIR__ . '/views/header.php';
+$allPosts = displayAllPosts($pdo);
+?>
 
 <!-- The view if the user is logged in -->
 <?php if (isset($_SESSION['user'])) : ?>
 
-    <?php require __DIR__ . '/app/display-posts.php'; ?>
     <h1>Photos feed</h1>
     <?php if (!$allPosts) : ?>
         <p>There is no photos...</p>
