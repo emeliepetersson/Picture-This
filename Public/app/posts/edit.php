60ada385f5908caa11cf68ef7b5fa3e73f377c29
Emelie Petersson <54805510+emeliepetersson@users.noreply.github.com>
2019-12-28 12:46:01 +0100

    update edit.php

--- Public/app/posts/edit.php
+++ Public/app/posts/edit.php
@@ -6,7 +6,28 @@ require __DIR__ . '/../autoload.php';
 
 // In this file we delete posts in the database.
 
-if (isset($_POST['description'])) {
+if (isset($_POST['post-id'], $_POST['description'])) {
+    $postId = filter_var($_POST['post-id'], FILTER_SANITIZE_NUMBER_INT);
+    $description = filter_var($_POST['description'], FILTER_SANITIZE_STRING);
+
+    $statement = $pdo->prepare('UPDATE posts
+    SET description = :description
+    WHERE id = :id AND user_id = :user_id');
+
+    if (!$statement) {
+        die(var_dump($pdo->errorInfo()));
+    }
+
+    $statement->bindParam(':description', $description, PDO::PARAM_STR);
+    $statement->bindParam(':id', $postId, PDO::PARAM_INT);
+    $statement->bindParam(':user_id',  $_SESSION['user']['id'], PDO::PARAM_INT);
+    $statement->execute();
+
+    $messages[] = "The post has been updated!";
+
+    if (count($messages) > 0) {
+        $_SESSION['messages'] = $messages;
+    }
 }
 
 redirect('/profile.php');
