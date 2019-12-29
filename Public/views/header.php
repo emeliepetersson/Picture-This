9b5b53c0b0bc0ba40f8ab95b8b538b292a41c2a2
emeliepetersson <emepet1204@skola.goteborg.se>
2019-12-29 13:38:16 +0100

    add like-button.js

--- Public/views/header.php
+++ Public/views/header.php
@@ -14,10 +14,10 @@ require __DIR__ . '/../app/messages.php';
     <title>Picture This</title>
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="/assets/styles/main.css">
     <link rel="stylesheet" href="/assets/styles/bottom-bar.css">
     <link rel="stylesheet" href="/assets/styles/upload-post.css">
     <link rel="stylesheet" href="/assets/styles/like-button.css">
+    <link rel="stylesheet" href="/assets/styles/main.css">
 </head>
 
 <body>
