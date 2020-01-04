6afcfefd45b5bcec60588818123c2553915493ef
Emelie Petersson <54805510+emeliepetersson@users.noreply.github.com>
2020-01-04 12:54:05 +0100

    add location to like.php and dislike.php

--- Public/app/posts/like.php
+++ Public/app/posts/like.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 require __DIR__ . '/../autoload.php';
 
+
 // In this file we add likes to posts in the database.
 
 
@@ -32,4 +33,4 @@ if (isset($_GET['id'])) {
     }
 }
 
-redirect('/profile.php');
+redirect('/' . $_GET['location']);
