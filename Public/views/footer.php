4870358489416f526e4aded2ac32dcda6f5b7a8c
Emelie Petersson <54805510+emeliepetersson@users.noreply.github.com>
2020-01-06 19:10:48 +0100

    add hamburger menu

--- Public/views/footer.php
+++ Public/views/footer.php
@@ -3,6 +3,7 @@
 <script src="/assets/scripts/main.js"></script>
 <script src="/assets/scripts/edit-post.js"></script>
 <script src="/assets/scripts/preview-image.js"></script>
+<script src="/assets/scripts/hamburger-menu.js"></script>
 </body>
 
 </html>
--- Public/views/header.php
+++ Public/views/header.php
@@ -14,6 +14,7 @@ require __DIR__ . '/../app/messages.php';
     <title>Picture This</title>
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="/assets/styles/navigation.css">
     <link rel="stylesheet" href="/assets/styles/bottom-bar.css">
     <link rel="stylesheet" href="/assets/styles/upload-post.css">
     <link rel="stylesheet" href="/assets/styles/like-button.css">
