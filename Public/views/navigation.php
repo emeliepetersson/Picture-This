f8b6a96c0a73adffffd351cd3511954f5008e97d
emeliepetersson <emepet1204@skola.goteborg.se>
2020-01-06 19:25:19 +0100

    Update navigation.php

--- Public/views/navigation.php
+++ Public/views/navigation.php
@@ -9,31 +9,31 @@
 
     <div class="menu">
         <div class="menu-items">
-            <ul class="navbar-nav">
+            <ul>
                 <?php if (isset($_SESSION['user'])) : ?>
-                    <li class="nav-item">
+                    <li>
                         <a class="nav-link" href="/profile.php">Profile</a>
                     </li><!-- /nav-item -->
-                    <li class="nav-item">
+                    <li>
                         <a class="nav-link" href="/settings.php">Settings</a>
                     </li><!-- /nav-item -->
-                    <li class="nav-item">
+                    <li>
                         <a class="nav-link" href="/upload.php">Upload post</a>
                     </li><!-- /nav-item -->
-                    <li class="nav-item">
+                    <li>
                         <a class="nav-link" href="/app/users/logout.php">Logout</a>
                     </li><!-- /nav-item -->
                 <?php else : ?>
-                    <li class="nav-item">
+                    <li>
                         <a class="nav-link" href="/index.php">Home</a>
                     </li><!-- /nav-item -->
-                    <li class="nav-item">
+                    <li>
                         <a class="nav-link" href="/about.php">About</a>
                     </li><!-- /nav-item -->
-                    <li class="nav-item">
+                    <li>
                         <a class="nav-link <?php echo $_SERVER['SCRIPT_NAME'] === '/login.php' ? 'active' : ''; ?>" href="login.php">Login</a>
                     </li><!-- /nav-item -->
-                    <li class="nav-item">
+                    <li>
                         <a class="nav-link" href="/signup.php">Sign up</a>
                     </li><!-- /nav-item -->
                 <?php endif; ?>
