523d5ee266611b35287f1d0c07eefbf1de517ed1
emeliepetersson <emepet1204@skola.goteborg.se>
2020-01-07 17:00:44 +0100

    add styling with css

--- Public/views/header.php
+++ Public/views/header.php
@@ -22,6 +22,7 @@ require __DIR__ . '/../app/messages.php';
     <link rel="stylesheet" href="/assets/styles/main.css">
     <link rel="stylesheet" href="/assets/styles/form.css">
     <link rel="stylesheet" href="/assets/styles/post.css">
+    <link rel="stylesheet" href="/assets/styles/profile.css">
     <link href="https://fonts.googleapis.com/css?family=Chilanka|Gayathri|Raleway|Roboto&display=swap" rel="stylesheet">
 </head>
 
