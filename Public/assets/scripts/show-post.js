126c1b2e8583bd427170f4048e17ea234758eda9
emeliepetersson <emepet1204@skola.goteborg.se>
2020-01-11 18:27:34 +0100

    update show-post.js

--- Public/assets/scripts/show-post.js
+++ Public/assets/scripts/show-post.js
@@ -10,5 +10,15 @@ function showPost(event) {
   const post = event.currentTarget.nextElementSibling;
   const background = document.querySelector(".background");
   background.classList.add("background-show");
-  post.classList.add("show");
+  post.classList.add("full-post-show");
+
+  const closeButton = post.childNodes[1];
+  closeButton.addEventListener("click", closePost);
+}
+
+function closePost(event) {
+  const post = event.currentTarget.parentNode;
+  const background = document.querySelector(".background");
+  background.classList.remove("background-show");
+  post.classList.remove("full-post-show");
 }
