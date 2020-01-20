378270efe9e3018c4a0fe8c49cf41ed81daaf51b
vehx <delta222@gmail.com>
2020-01-20 16:01:30 +0100

    added some comment creation logic

--- Public/app/comments/create.php
+++ Public/app/comments/create.php
@@ -5,3 +5,32 @@ declare(strict_types=1);
 require __DIR__ . '/../autoload.php';
 
 // here we add a comment to a post
+
+isLoggedIn();
+
+if (isset($_POST['comment'], $_POST['post_id'], $_SESSION['user'])) {
+    $comment = filter_var($_POST['comment'], FILTER_SANITIZE_STRING);
+    $postId = filter_var($_POST['post_id'], FILTER_SANITIZE_NUMBER_INT);
+    $userId = $_SESSION['user']['id'];
+
+    $query = 'INSERT INTO comments (post_id, user_id, comment, date) VALUES (:post_id, :user_id, :comment, :date)';
+
+    $statement = $pdo->prepare($query);
+
+    if (!$statement) {
+        die(var_dump($pdo->errorInfo()));
+    }
+
+    $statement->bindParam(':post_id', $postId, PDO::PARAM_INT);
+    $statement->bindParam(':user_id', $userId, PDO::PARAM_INT);
+    $statement->bindParam(':comment', $comment, PDO::PARAM_STR);
+    $statement->bindParam(':date', date('d-m-Y, H:i:s'), PDO::PARAM_STR);
+
+    $statement->execute();
+
+    $messages[] = "Your comment have been successfully posted!";
+
+    $_SESSION['messages'] = $messages;
+} else {
+    redirect('/');
+}
