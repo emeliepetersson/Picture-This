0415f11b45e5d1c3d71f75af8925c6acee9146db
vehx <delta222@gmail.com>
2020-01-21 18:38:17 +0100

    added more to the fetch post layout still WIP

--- Public/assets/scripts/search.js
+++ Public/assets/scripts/search.js
@@ -21,58 +21,86 @@ if (searchInput) {
       body: searchData
     })
       .then(response => response.json())
-      .then(posts => {
+      .then(usersPosts => {
         searchResults.innerHTML = "";
 
-        // console.log(posts);
+        console.log(usersPosts);
 
-        if (posts != "No posts found") {
-          posts.forEach(post => {
-            console.log(post);
+        if (usersPosts != "No posts found") {
+          usersPosts.forEach(userPosts => {
+            console.log(userPosts);
 
-            if (post.profile_image === "") {
-              post.profile_image = "images/profile-picture.png";
-            }
+            userPosts.forEach(post => {
+              let likeImage;
 
-            const template = `
-            <div>
-            <header>
-            <img class="profile-image" src="/<?php echo $post['profile_image'] ? 'uploads/' . $post['profile_image'] : 'images/profile-picture.png' ?>" alt="profile image" loading="lazy">
-            <?php if ((int) $post['user_id'] === $_SESSION['user']['id']) : ?>
-                <a href="/profile.php">
-                    <h2>
-                        ${post.first_name} ${post.last_name}
-                    </h2>
-                </a>
-            <?php else : ?>
-                <a href="/user-profiles.php?user-id=${post.user_id}">
-                    <h2>
-                        ${post.first_name} ${post.last_name}
-                    </h2>
-                </a>
-            <?php endif; ?>
-            <p class="date">${post.date}</p>
-        </header>
-        <div class="post-image">
-            <img src="/uploads/${post.image}" alt="uploaded post" loading="lazy">
-        </div>
-        
-        
-        <div class="caption-container">
-            likes
-            <?php
-            $postIsliked = getDataWithTwoConditions($pdo, "post_id, user_id", "likes", "post_id", "user_id", (int) $post['id'], $_SESSION['user']['id']);
-            $amountOfLikes = count(getDataAsArrayFromTable($pdo, "post_id", "likes", "post_id", $post['id']));
-            ?>
-            amount
-            <div class="likes-container">
-                likes <?php require __DIR__ . '/like-form.php'; ?>
-            </div>
-            <p class="caption"><span class="bold">${post.first_name} ${post.last_name} </span> ${post.description}</p>
-        </div>
-        </div>
-        `;
-            searchResults.innerHTML += template;
+              if (post.profile_image === "") {
+                post.profile_image = "images/profile-picture.png";
+              } else {
+                post.profile_image = "uploads/" + post.profile_image;
+              }
+
+              if (post.like === "liked") {
+                likeImage = "/images/like.svg";
+              } else {
+                likeImage = "/images/dislike.svg";
+              }
+
+              const template = `
+              <article class="post">
+                  <div class="post-image thumbnail-post">
+                      <img src="/uploads/${post.image}" alt="uploaded post" loading="lazy">
+                  </div>
+                  <div class="full-post">
+                      <div class="close-button-container">
+                          <button class="close-button" type="button"><img class="close" src="/images/close.png" alt="close icon"></button>
+                      </div>
+                      <div>
+                          <header>
+                              <img class="profile-image" src="/${post.profile_image}" alt="profile image" loading="lazy">
+                              <?php if ((int) $post['user_id'] === $_SESSION['user']['id']) : ?>
+                                  <a href="/profile.php">
+                                      <h2>
+                                          ${post.first_name} ${post.last_name}
+                                      </h2>
+                                  </a>
+                              <?php else : ?>
+                                  <a href="/user-profiles.php?user-id=${post.user_id}">
+                                      <h2>
+                                          ${post.first_name} ${post.last_name}
+                                      </h2>
+                                  </a>
+                              <?php endif; ?>
+                              <p class="date">${post.date}</p>
+                          </header>
+                          <div class="post-image">
+                              <img src="/uploads/${post.image}" alt="uploaded post" loading="lazy">
+                          </div>
+
+                          <div class="caption-container">
+                              <?php
+                              $postIsliked = getDataWithTwoConditions($pdo, "post_id, user_id", "likes", "post_id", "user_id", (int) $post['id'], $_SESSION['user']['id']);
+                              $amountOfLikes = count(getDataAsArrayFromTable($pdo, "post_id", "likes", "post_id", $post['id']));
+                              ?>
+                              <div class="likes-container">
+                                  <form class="like-form" method="post">
+                                      <input type="text" name="post-id" value="${post.id}" hidden>
+                                      <button type="submit" class="like-form-button ${post.like}"><img src="${likeImage}" alt="like button"></button>
+                                  </form>
+                                  <p class="like-counter">${post.likes}</p>
+                                  <button class="comment-btn">Comment</button>
+                              </div>
+                              <p class="caption"><span class="bold">${post.first_name} ${post.last_name} </span> ${post.description}</p>
+                          </div>
+                      </div>
+              </article>
+              `;
+              searchResults.innerHTML += template;
+            });
+
+            const thumbnails = document.querySelectorAll(".thumbnail-post");
+            thumbnails.forEach(thumbnail => {
+              thumbnail.addEventListener("click", showPost);
+            });
           });
         }
       })
