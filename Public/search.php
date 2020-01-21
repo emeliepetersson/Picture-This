1c038cea878729e96d6237b1b69780cd17bec88b
vehx <delta222@gmail.com>
2020-01-21 18:51:04 +0100

    added more logic for search

--- Public/search.php
+++ Public/search.php
@@ -4,18 +4,16 @@
 <article>
 
 
-    <?php require __DIR__ . '/views/errors.php'; ?>
+    <?php require __DIR__ . '/views/messages.php'; ?>
 
     <div class="form-group">
         <input type="search" name="search" class="search-input" placeholder="Enter search here." autocomplete="off">
     </div>
 
-    <div class="search-results"></div>
+    <div class="background"></div>
+    <div class="explore-container search-results"></div>
 
 </article>
 
 <?php require __DIR__ . '/views/bottom-bar.php'; ?>
-
-<script src="assets/scripts/search.js"></script>
-
 <?php require __DIR__ . '/views/footer.php'; ?>
