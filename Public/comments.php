772033657695ca20cbd21056b2f8ebb40f124115
vehx <delta222@gmail.com>
2020-01-23 18:54:28 +0100

    added layout of comments page

--- Public/comments.php
+++ Public/comments.php
@@ -1,7 +1,15 @@
 <?php require __DIR__ . '/views/header.php'; ?>
 <?php isLoggedIn(); ?>
 
-<!-- todo layout/logic to show posts comments -->
+<div class="comment-new">
+    <label for="comment-content" hidden>Create comment</label>
+    <input class="comment-content" name="comment-content" id="comment-content" type="text">
+    <input type="hidden" name="user-id" id="user-id" value="<?php echo $_SESSION['user']['id'] ?>">
+    <input type="hidden" name="post-id" id="post-id" value="<?php echo $_GET['post'] ?>">
+    <button class="comment-submit"><img src="/images/upload.svg" alt="Comment"></button>
+</div>
+
+<div class="comment-container"></div>
 
 <?php require __DIR__ . '/views/bottom-bar.php'; ?>
 <?php require __DIR__ . '/views/footer.php'; ?>
