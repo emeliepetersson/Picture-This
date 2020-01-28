0fd956b2eb6faf9c1e5be2c65df31e394cbaa079
vehx <delta222@gmail.com>
2020-01-28 19:55:40 +0100

    added more logic to comment layout and functions

--- Public/comments.php
+++ Public/comments.php
@@ -6,10 +6,9 @@
     <input class="comment-content" name="comment-content" id="comment-content" type="text">
     <input type="hidden" name="user-id" id="user-id" value="<?php echo $_SESSION['user']['id'] ?>">
     <input type="hidden" name="post-id" id="post-id" value="<?php echo $_GET['post'] ?>">
-    <button class="comment-submit"><img src="/images/upload.svg" alt="Comment"></button>
+    <button class="button smaller-button comment-submit">Submit comment</button>
 </div>
-<input type="hidden" name="edit-comment-id" id="edit-comment-id" value="">
-<input type="text" name="edit-comment-content" id="edit-comment-content" value="">
+
 <div class="comment-container"></div>
 
 <?php require __DIR__ . '/views/bottom-bar.php'; ?>
