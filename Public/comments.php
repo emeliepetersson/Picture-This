03412f419934cb8d754b3c2a6e2978ae5bb5646b
vehx <delta222@gmail.com>
2020-01-29 12:45:21 +0100

    added placeholder for clarity

--- Public/comments.php
+++ Public/comments.php
@@ -1,9 +1,11 @@
 <?php require __DIR__ . '/views/header.php'; ?>
 <?php isLoggedIn(); ?>
 
+<?php require __DIR__ . '/views/messages.php'; ?>
+
 <div class="comment-new">
     <label for="comment-content" hidden>Create comment</label>
-    <input class="comment-content" name="comment-content" id="comment-content" type="text">
+    <input class="comment-content" name="comment-content" id="comment-content" placeholder="Type comment here" type="text">
     <input type="hidden" name="user-id" id="user-id" value="<?php echo $_SESSION['user']['id'] ?>">
     <input type="hidden" name="post-id" id="post-id" value="<?php echo $_GET['post'] ?>">
     <button class="button smaller-button comment-submit">Submit comment</button>
